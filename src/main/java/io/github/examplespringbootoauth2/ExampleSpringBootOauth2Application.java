598ccc8464d1b402b3741966996e81b3fd3646f4
Julio Amorim <julio.l.amorim00@gmail.com>
2020-05-20 15:23:03 -0300

    First commit, and more: Index file added, dependencies from bootstrap and jQuery added

---
+++ src/main/java/io/github/examplespringbootoauth2/ExampleSpringBootOauth2Application.java
@@ -0,0 +1,13 @@
+package io.github.examplespringbootoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/examplespringbootoauth2/ExampleSpringBootOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.examplespringbootoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBootOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
