8381c05e6b45973cd056cc70701a9079642ecf10
Julio Amorim <julio.l.amorim00@gmail.com>
2020-05-20 20:02:14 -0300

    Divs fo content in state authenticated and unauthenticated added

--- src/main/java/io/github/examplespringbootoauth2/ExampleSpringBootOauth2Application.java
+++ src/main/java/io/github/examplespringbootoauth2/ExampleSpringBootOauth2Application.java
@@ -1,13 +1,47 @@
 package io.github.examplespringbootoauth2;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ExampleSpringBootOauth2Application {
+@RestController
+public class ExampleSpringBootOauth2Application extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleSpringBootOauth2Application.class, args);
+	}
+
+	// ...
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		 http
+			 .authorizeRequests(a -> a
+				 .antMatchers("/", "/error", "/webjars/**").permitAll()
+				 .anyRequest().authenticated()
+			 )
+			 .exceptionHandling(e -> e
+				 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+			 )
+			 .oauth2Login();
+		 // @formatter:on
 	}
 
 }
